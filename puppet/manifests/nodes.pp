6c0812462865061d13cf539d2be2a3c05aed08c7
Roman Heinrich <roman.heinrich@gmail.com>
2013-08-20 01:32:34 +0200

    remove default node

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -9,16 +9,14 @@ class common{
   class { 'resolv':}
 }
 
-# node default{
-#   #class{"glusterfs":}
-# }
-
 node "glusterserver1" {
   class{"common":}
   -> file{"/tmp/glusterserver1": ensure => present}
+  -> class{"glusterfs":}
 }
 
 node "glusterserver2" {
   class{"common":}
   -> file{"/tmp/glusterserver2": ensure => present}
+  -> class{"glusterfs":}
 }
